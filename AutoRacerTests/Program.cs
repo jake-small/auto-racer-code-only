dfc85893d80575f73b1f6644720a1b8e3f3d3af7
jake-small <jakesmall@protonmail.com>
2022-01-09 20:07:50 -0800

    unit test setup

---
+++ AutoRacerTests/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AutoRacerTests
+{
+  [TestFixture]
+  public class PrimeService_IsPrimeShould
+  {
+    [SetUp]
+    public void SetUp()
+    {
+    }
+
+    [Test]
+    public void IsPrime_InputIs1_ReturnFalse()
+    {
+      Assert.Pass();
+    }
+  }
+}
