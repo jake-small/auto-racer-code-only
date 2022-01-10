0e4ca5fe58de0c8d6482e1042fabcbe8dc094544
jake-small <jakesmall@protonmail.com>
2022-01-09 20:42:51 -0800

    unit test ref project

---
+++ AutoRacerTests/tests/CalculationLayerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AutoRacerTests.Tests
+{
+  [TestFixture]
+  public class CalculationLayerTests
+  {
+    private CalculationLayer _calcLayer = new CalculationLayer();
+
+    [SetUp]
+    public void SetUp()
+    {
+    }
+
+    [Test]
+    public void ApplyLevelValues_All_Success()
+    {
+      var card = GetTestCard();
+      Assert.That(card.GetRawName(), Is.EqualTo("TestCard"));
+    }
+
+    private Card GetTestCard()
+    {
+      return new Card
+      {
+        Name = "TestCard"
+      };
+    }
+  }
+}
