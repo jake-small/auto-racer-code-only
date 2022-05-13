d7eb309a5f0661603b47350c622c1025f063d47e
jake-small <jakesmall@protonmail.com>
2022-05-13 15:43:52 -0700

    sine curve movement

--- src/scripts/race/effects/Projectile.cs
+++ src/scripts/race/effects/Projectile.cs
@@ -4,8 +4,10 @@ using System;
 public class Projectile : Area2D
 {
   public Vector2 Target { get; set; }
-  private const float Speed = 160;
-  private Vector2 _velocity;
+  private const float Speed = 260;
+  private float _time = 0;
+  private const float TowardsStrength = 0.5f;
+  private const float PerpendicularStrength = 0.5f;
 
   public override void _Ready()
   {
@@ -17,15 +19,35 @@ public class Projectile : Area2D
     Position = new Vector2(Position.x + (offsetX * directionX), Position.y + (offsetY * directionY));
   }
 
-  public override void _PhysicsProcess(float delta)
+  public override void _Process(float delta)
   {
     if (Target != null && Target != Position)
     {
-      Position = Position.MoveToward(Target, delta * Speed);
+      _time += delta;
+      var towardsTarget = (Target - Position).Normalized();
+      var perpendicular = new Vector2(towardsTarget.y, -towardsTarget.x);
+      Position += (TowardsStrength * towardsTarget + PerpendicularStrength * perpendicular * (float)Math.Sin(_time)) * Speed * delta;
+      if (Math.Abs(Target.x - Position.x) < 5 && Math.Abs(Target.y - Position.y) < 5)
+      {
+        QueueFree();
+      }
     }
     else if (Target != null && Target == Position)
     {
       QueueFree();
     }
+  }
+
+  private Vector2 CubicBezier(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3, float t)
+  {
+    Vector2 q0 = p0.LinearInterpolate(p1, t);
+    Vector2 q1 = p1.LinearInterpolate(p2, t);
+    Vector2 q2 = p2.LinearInterpolate(p3, t);
+
+    Vector2 r0 = q0.LinearInterpolate(q1, t);
+    Vector2 r1 = q1.LinearInterpolate(q2, t);
+
+    Vector2 s = r0.LinearInterpolate(r1, t);
+    return s;
   }
 }
