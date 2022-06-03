5fee05341ac41f8416bd8c089d27df6cd54ba2ec
jake-small <jakesmall@protonmail.com>
2022-06-02 23:26:16 -0700

    prep ability targetting

--- src/engine/PrepEngine.cs
+++ src/engine/PrepEngine.cs
@@ -140,45 +140,152 @@ public class PrepEngine
   {
     var targets = new List<Card>();
     var target = ability.Target;
+    var targetInventoryType = target.GetInventoryType();
+    var cardSlot = PlayerInventory.GetSlotOfCard(card);
     switch (target.GetTargetType())
     {
       case TargetType.Self:
-        if (target.GetInventoryType() == InventoryType.Any || target.GetInventoryType() == card.InventoryType)
+        if (targetInventoryType == InventoryType.Any || targetInventoryType == card.InventoryType)
         {
           targets.Add(card);
         }
         break;
       case TargetType.Others:
-        if (target.GetInventoryType() == InventoryType.Any)
+        if (targetInventoryType == InventoryType.Any)
         {
           targets.AddRange(PlayerInventory.GetCardsAsList().Where(c => c != card));
           targets.AddRange(ShopInventory.GetCardsAsList().Where(c => c != card));
         }
-        else if (target.GetInventoryType() == InventoryType.Player)
+        else if (targetInventoryType == InventoryType.Player)
         {
-          targets.AddRange(PlayerInventory.GetCardsAsList().Where(c => c != card));
+          var otherPlayerCardsDict = PlayerInventory.GetCards();
+          if (card.InventoryType == targetInventoryType && target.GetPriority() == Priority.Closest)
+          {
+            var otherPlayerCards = otherPlayerCardsDict.OrderBy(e => Math.Abs(e.Key - cardSlot)).Where(c => c.Value != card).Select(d => d.Value);
+            targets.AddRange(otherPlayerCards);
+          }
+          else if (card.InventoryType == targetInventoryType && target.GetPriority() == Priority.Furthest)
+          {
+            var otherPlayerCards = otherPlayerCardsDict.OrderByDescending(e => Math.Abs(e.Key - cardSlot)).Where(c => c.Value != card).Select(d => d.Value);
+            targets.AddRange(otherPlayerCards);
+          }
+          else if (target.GetPriority() == Priority.PositionAscending)
+          {
+            var otherPlayerCards = otherPlayerCardsDict.OrderBy(d => d.Key).Where(c => c.Value != card).Select(d => d.Value);
+            targets.AddRange(otherPlayerCards);
+          }
+          else if (target.GetPriority() == Priority.PositionDescending)
+          {
+            var otherPlayerCards = otherPlayerCardsDict.OrderByDescending(d => d.Key).Where(c => c.Value != card).Select(d => d.Value);
+            targets.AddRange(otherPlayerCards);
+          }
+          else
+          {
+            var otherPlayerCards = PlayerInventory.GetCardsAsList().Where(c => c != card).ToList();
+            targets.AddRange(otherPlayerCards);
+          }
         }
         else if (target.GetInventoryType() == InventoryType.Shop)
         {
-          targets.AddRange(ShopInventory.GetCardsAsList().Where(c => c != card));
+          var otherShopCardsDict = ShopInventory.GetCards();
+          if (card.InventoryType == targetInventoryType && target.GetPriority() == Priority.Closest)
+          {
+            var otherShopCards = otherShopCardsDict.OrderBy(e => Math.Abs(e.Key - cardSlot)).Where(c => c.Value != card).Select(d => d.Value);
+            targets.AddRange(otherShopCards);
+          }
+          else if (card.InventoryType == targetInventoryType && target.GetPriority() == Priority.Furthest)
+          {
+            var otherShopCards = otherShopCardsDict.OrderByDescending(e => Math.Abs(e.Key - cardSlot)).Where(c => c.Value != card).Select(d => d.Value);
+            targets.AddRange(otherShopCards);
+          }
+          else if (target.GetPriority() == Priority.PositionAscending)
+          {
+            var otherShopCards = otherShopCardsDict.OrderBy(d => d.Key).Where(c => c.Value != card).Select(d => d.Value);
+            targets.AddRange(otherShopCards);
+          }
+          else if (target.GetPriority() == Priority.PositionDescending)
+          {
+            var otherShopCards = otherShopCardsDict.OrderByDescending(d => d.Key).Where(c => c.Value != card).Select(d => d.Value);
+            targets.AddRange(otherShopCards);
+          }
+          else
+          {
+            var otherShopCards = ShopInventory.GetCardsAsList().Where(c => c != card).ToList();
+            targets.AddRange(otherShopCards);
+          }
         }
         break;
       case TargetType.All:
-        if (target.GetInventoryType() == InventoryType.Any)
+        if (targetInventoryType == InventoryType.Any)
         {
           targets.AddRange(PlayerInventory.GetCardsAsList());
           targets.AddRange(ShopInventory.GetCardsAsList());
         }
-        else if (target.GetInventoryType() == InventoryType.Player)
+        else if (targetInventoryType == InventoryType.Player)
         {
-          targets.AddRange(PlayerInventory.GetCardsAsList());
+          var otherPlayerCardsDict = PlayerInventory.GetCards();
+          if (card.InventoryType == targetInventoryType && target.GetPriority() == Priority.Closest)
+          {
+            var otherPlayerCards = otherPlayerCardsDict.OrderBy(e => Math.Abs(e.Key - cardSlot)).Select(d => d.Value);
+            targets.AddRange(otherPlayerCards);
+          }
+          else if (card.InventoryType == targetInventoryType && target.GetPriority() == Priority.Furthest)
+          {
+            var otherPlayerCards = otherPlayerCardsDict.OrderByDescending(e => Math.Abs(e.Key - cardSlot)).Select(d => d.Value);
+            targets.AddRange(otherPlayerCards);
+          }
+          else if (target.GetPriority() == Priority.PositionAscending)
+          {
+            var otherPlayerCards = otherPlayerCardsDict.OrderBy(d => d.Key).Select(d => d.Value);
+            targets.AddRange(otherPlayerCards);
+          }
+          else if (target.GetPriority() == Priority.PositionDescending)
+          {
+            var otherPlayerCards = otherPlayerCardsDict.OrderByDescending(d => d.Key).Select(d => d.Value);
+            targets.AddRange(otherPlayerCards);
+          }
+          else
+          {
+            var otherPlayerCards = PlayerInventory.GetCardsAsList().ToList();
+            targets.AddRange(otherPlayerCards);
+          }
         }
         else if (target.GetInventoryType() == InventoryType.Shop)
         {
-          targets.AddRange(ShopInventory.GetCardsAsList());
+          var otherShopCardsDict = ShopInventory.GetCards();
+          if (card.InventoryType == targetInventoryType && target.GetPriority() == Priority.Closest)
+          {
+            var otherShopCards = otherShopCardsDict.OrderBy(e => Math.Abs(e.Key - cardSlot)).Select(d => d.Value);
+            targets.AddRange(otherShopCards);
+          }
+          else if (card.InventoryType == targetInventoryType && target.GetPriority() == Priority.Furthest)
+          {
+            var otherShopCards = otherShopCardsDict.OrderByDescending(e => Math.Abs(e.Key - cardSlot)).Select(d => d.Value);
+            targets.AddRange(otherShopCards);
+          }
+          else if (target.GetPriority() == Priority.PositionAscending)
+          {
+            var otherShopCards = otherShopCardsDict.OrderBy(d => d.Key).Select(d => d.Value);
+            targets.AddRange(otherShopCards);
+          }
+          else if (target.GetPriority() == Priority.PositionDescending)
+          {
+            var otherShopCards = otherShopCardsDict.OrderByDescending(d => d.Key).Select(d => d.Value);
+            targets.AddRange(otherShopCards);
+          }
+          else
+          {
+            var otherShopCards = ShopInventory.GetCardsAsList().ToList();
+            targets.AddRange(otherShopCards);
+          }
         }
         break;
     }
-    return targets;
+
+    if (target.Amount.ToInt() > targets.Count)
+    {
+      return targets;
+    }
+    return targets.Take(target.Amount.ToInt());
   }
 }
